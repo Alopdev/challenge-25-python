7fedb4c8341714deb799a53552a354e6c2c02d8e
Alopdev <alelag18@gmail.com>
2026-09-03 11:23:52 +0200

    Corrigiendo fallos y solventando lio mental de listas anidadas

--- reto_02.py
+++ reto_02.py
@@ -14,7 +14,13 @@ Nivel: parecido al"""
 meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12]
 gastos_mes = []
 gasto ={} #gasto sería un diccionario que tendría todos los gastos
-categorias = ["Ocio", "Hogar", "Banco", "Alimentacion"]
+categorias = ["Ocio", "Hogar", "Banco", "Alimentación"]
+indice = 0
+
+for i in range(12):
+    gastos_mes.append([])
+
+   
 
 def define_categoría():
     categories = input("Escoge una categoría: Ocio, Hogar, Banco o Alimentación :\n")
@@ -22,23 +28,33 @@ def define_categoría():
         return categories.title()
     else:
         print("No existe esa categoría")
-        define_categoría()
+        return define_categoría()
 
 
 def add_gastos():
     descripcion = input("Concepto de gasto: ")
     categoria = define_categoría()
     cantidad = int(input("Cantidad: "))
-    gasto = {"Decripción: ": descripcion, "Categoría: ": categoria, "Cantidad": cantidad}
+    gasto = {"Descripcion": descripcion, "Categoría": categoria, "Cantidad": cantidad}
     return(gasto)
 
-def gasto_mes(gasto):
-    mes = int("Introduce el mes en el que se ha producido el gasto (del 1 al 12): ")
+def choose_mes():
+    mes = int(input("Introduce el mes en el que se ha producido el gasto (del 1 al 12): "))
     if mes >=1 and mes <=12:
-        gasto_mes[mes-1].append(add_gastos)
+        return mes
     else:
         print("No existe ese mes")
-        gasto_mes()
-gastador = add_gastos()
-gasto_mes(gastador)
-print(gasto_mes[0])
+        return choose_mes()
+
+def create_gasto():
+    mes = choose_mes()
+    indice = mes - 1
+    print(indice)
+    gasto = add_gastos()
+    gastos_mes[indice].append(gasto)
+
+
+create_gasto()
+
+
+
