8a842e293f178363f250cf6ab3ecacb3207b61e5
Alopdev <alelag18@gmail.com>
2026-09-20 17:01:27 +0200

    Empezando a plantear el problema, lo dejo iniciado y trato de plasmarlo en papel primero

---
+++ reto_03.py
@@ -0,0 +1,27 @@
+""" Para el reto 3: Gestor de entrenamientos.
+
+Objetivo: crear un programa donde puedas registrar entrenamientos, consultar el historial, calcular estadísticas y buscar entrenamientos por tipo.
+
+Pero vamos a hacerlo como los anteriores: te doy los requisitos, no la solución.
+
+Añadir entrenamiento: fecha, tipo (Bici, Carrera, Natación, Gimnasio), duración y distancia.
+Ver todos los entrenamientos.
+Calcular tiempo total entrenado.
+Calcular distancia total por tipo de entrenamiento.
+Buscar entrenamientos por tipo.
+Salir.
+
+Dificultad: un pequeño salto respecto al reto 2, sobre todo por trabajar mejor con datos relacionados entre sí."""
+
+#función para añdir el entrenamiento
+def add_train():
+    fecha =input("Introduce la fecha del entrenamiento DD/MM/YYYY: ")
+    
+
+
+
+
+#función para calcular tiempo total entrenado
+#función para calcular distancia total por tipo de entrenamiento
+#función mostrar entrenamientos por tipo
+#Bucle app con la opción de salir
